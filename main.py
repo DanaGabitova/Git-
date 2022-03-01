c0873e7dde9c9a794eff72e5d027c888b35accb2
DanaGabitova <58876248+DanaGabitova@users.noreply.github.com>
2022-03-01 16:44:49 +0600

    add main and ui

---
+++ main.py
@@ -0,0 +1,40 @@
+import random
+import sys
+from PyQt5.QtGui import QPainter, QColor
+from PyQt5.QtWidgets import QWidget, QApplication, QPushButton
+
+
+class Circle(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        self.setGeometry(300, 300, 300, 300)
+        self.setWindowTitle('Git и желтые окружности')
+        self.btn = QPushButton(self)
+        self.btn.move(125, 150)
+        self.btn.resize(60, 40)
+        self.flag = False
+        self.btn.clicked.connect(self.paint)
+
+    def paintEvent(self, event):
+        if self.flag:
+            size = random.randint(1, 150)
+            qp = QPainter()
+            qp.begin(self)
+            qp.setBrush(QColor(255, 255, 0))
+            qp.drawEllipse(random.randint(0, 150),
+                           random.randint(0, 150), size, size)
+            qp.end()
+
+    def paint(self):
+        self.flag = True
+        self.repaint()
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = Circle()
+    ex.show()
+    sys.exit(app.exec())
