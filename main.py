ae6512eead2dd54bfab00cc337644ff10361d671
DanaGabitova <58876248+DanaGabitova@users.noreply.github.com>
2022-03-01 19:49:46 +0300

    добавила окружности

--- main.py
+++ main.py
@@ -1,30 +1,33 @@
 import random
 import sys
 from PyQt5.QtGui import QPainter, QColor
-from PyQt5.QtWidgets import QWidget, QApplication
-from ui_file import Ui_MainWindow
+from PyQt5.QtWidgets import QWidget, QApplication, QPushButton
 
 
-class Circle(QWidget, Ui_MainWindow):
+class Circle(QWidget):
     def __init__(self):
         super().__init__()
-        self.setupUi(self)
+        self.initUI()
 
+    def initUI(self):
+        self.setGeometry(300, 300, 300, 300)
+        self.setWindowTitle('Git и желтые окружности')
+        self.btn = QPushButton(self)
+        self.btn.move(125, 150)
+        self.btn.resize(60, 40)
         self.flag = False
-        self.pushButton.clicked.connect(self.paint)
+        self.btn.clicked.connect(self.paint)
 
     def paintEvent(self, event):
         if self.flag:
             size = random.randint(1, 150)
-            r = random.randint(0, 255)
-            g = random.randint(0, 255)
-            b = random.randint(0, 255)
             qp = QPainter()
-            qp.begin(self)
-            qp.setBrush(QColor(r, g, b))
-            qp.drawEllipse(random.randint(0, 150),
-                           random.randint(0, 150), size, size)
-            qp.end()
+            for i in range(1, random.randint(3, 6)):
+                qp.begin(self)
+                qp.setBrush(QColor(255, 255, 0))
+                qp.drawEllipse(random.randint(0, 150),
+                               random.randint(0, 150), size, size)
+                qp.end()
 
     def paint(self):
         self.flag = True
